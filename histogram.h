b05f5d27b7a4141c273399b80af2a448c35fe270
Maria (LopatinaMA) <LopatinaMA@mpei.ru>
2022-05-30 12:26:31 +0300

    code: перенос определения структуры

--- histogram.h
+++ histogram.h
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 
 void find_minmax(vector<double> numbers, double& min, double& max);
 bool proverka(string bin_colour);
